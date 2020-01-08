0f06432132a090ceb62295590d9d7b3ed7beffdf
bohaixv <1146873420@qq.com>
2020-01-08 11:51:36 +0800

    feat: initial

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 learn prolog to proficiency
 
 this is a repository for learning prolog.
+
+
+first : I will do all of the book's sample in the repository.
+
+second : I will find another book and forum to improve my ability.
+
+third : I will do some tools from prolog.
+
+come on.
---
+++ sample-from-book.pl
@@ -0,0 +1,20 @@
+# food_type(veleeta , cheese).
+# food_type(ritz, racker).
+# food_type(spam, meat).
+# food_type(sausage, meat).
+# food_type(jolt, soda).
+# food_type(twinkie, dessert).
+# flavor(sweet, dessert).
+# flavor(savory, meat).
+# flavor(savory, cheese).
+# flavor(sweet, soda).
+# food_flavor(X, Y) :- food_type(X, Z) , flavor(Y, Z).
+
+# cat(lion).
+# cat(tiger).
+
+# dorothy(X, Y, Z) :- X=lion, Y=tiger, Z=monkey.
+# twins_cat(X, Y) :- cat(X), cat(Y).
+
+
+
